044fdfccd642b0e5237455a8a83c1f23dc97f7ca
andres.mera <feli041089@gmail.com>
2023-12-07 09:27:50 -0500

    main : Creacion del proyecto de rick and morty.

---
+++ src/main/java/com/coderbyte/apprickmorty/AppRickmortyApplication.java
@@ -0,0 +1,13 @@
+package com.coderbyte.apprickmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRickmortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRickmortyApplication.class, args);
+	}
+
+}
