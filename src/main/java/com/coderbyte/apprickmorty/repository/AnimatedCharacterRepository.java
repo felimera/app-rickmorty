a3b0950391765caef232bc801ec402dda264fdc6
andres.mera <feli041089@gmail.com>
2023-12-07 11:35:47 -0500

    dev : Creando el reposirotio de los personaje animados.

--- src/main/java/com/coderbyte/apprickmorty/repository/AnimatedCharacterRepository.java
+++ src/main/java/com/coderbyte/apprickmorty/repository/AnimatedCharacterRepository.java
@@ -1,9 +1,9 @@
 package com.coderbyte.apprickmorty.repository;
 
-import com.coderbyte.apprickmorty.model.Character;
+import com.coderbyte.apprickmorty.model.AnimatedCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CharacterRepository extends JpaRepository<Integer, Character> {
+public interface AnimatedCharacterRepository extends JpaRepository<AnimatedCharacter, Integer> {
 }
