1221417d3351372ca152ca28e405371208775414
andres.mera <feli041089@gmail.com>
2023-12-07 11:00:34 -0500

    dev : Modificacion en la BD.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,27 @@
 drop table if exists characters;
 drop table if exists called_tables;
 
-CREATE TABLE characters (
-    ch_id int auto_increment  primary key,
+create table characters (
+    ch_id int auto_increment primary key,
     ch_name varchar(50) not null,
     ch_picture varchar(50) not null,
     ch_gender varchar(2) not null,
     ch_state varchar(2) null
 );
 
-CREATE TABLE called_tables (
-    ct_id int auto_increment  primary key,
-    ct_date date not null,
+create table system_errors (
+    se_id int auto_increment primary key,
+    se_code varchar(10) not null,
+    se_message varchar(50) not null
+);
+
+create table called_tables (
+    ct_id int auto_increment primary key,
+    ct_registration_date date not null,
     ct_type_request varchar(4) not null,
     ct_login_information varchar(1000) not null,
-    ct_error varchar(50) null
+    ct_error_id int null
 );
+
+
+alter table called_tables add foreign key (ct_error_id) references system_errors(se_id);
