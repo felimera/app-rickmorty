647623bd8d6b8ea8abd6c14a9861203cd44f6409
andres.mera <feli041089@gmail.com>
2023-12-07 14:22:02 -0500

    dev : Modificacion en e pom para utilizar el lombok con mapStruct.

--- src/main/java/com/coderbyte/apprickmorty/model/AnimatedCharacter.java
+++ src/main/java/com/coderbyte/apprickmorty/model/AnimatedCharacter.java
@@ -1,7 +1,11 @@
 package com.coderbyte.apprickmorty.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "characters")
 public class AnimatedCharacter {
@@ -17,55 +21,4 @@ public class AnimatedCharacter {
     private String gender;
     @Column(name = "ch_state")
     private String state;
-
-    public AnimatedCharacter() {
-    }
-
-    public AnimatedCharacter(Integer id, String name, String picture, String gender, String state) {
-        this.id = id;
-        this.name = name;
-        this.picture = picture;
-        this.gender = gender;
-        this.state = state;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    public void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 }
