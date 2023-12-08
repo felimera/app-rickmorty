4cf8f0fe9258b00d98cf557aed3098a35dfcc0dc
andres.mera <feli041089@gmail.com>
2023-12-08 10:07:32 -0500

    dev : Creacion del endpoint y del mapper para la paginacion.

--- src/main/java/com/coderbyte/apprickmorty/controller/AnimatedCharacterController.java
+++ src/main/java/com/coderbyte/apprickmorty/controller/AnimatedCharacterController.java
@@ -1,10 +1,12 @@
 package com.coderbyte.apprickmorty.controller;
 
 import com.coderbyte.apprickmorty.controller.dto.AnimatedCharacterDTO;
+import com.coderbyte.apprickmorty.controller.dto.parameter.PageableSearchDTO;
 import com.coderbyte.apprickmorty.controller.mapper.AnimatedCharacterMapper;
 import com.coderbyte.apprickmorty.model.AnimatedCharacter;
 import com.coderbyte.apprickmorty.service.AnimatedCharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,5 +34,20 @@ public class AnimatedCharacterController {
     public ResponseEntity<Object> postAnimatedCharacter(@RequestBody AnimatedCharacterDTO dto) {
         AnimatedCharacter animatedCharacter = AnimatedCharacterMapper.INSTANCE.toEntity(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(animatedCharacterService.postAnimatedCharacter(animatedCharacter));
+    }
+
+    @GetMapping(path = "/all/pageable")
+    public ResponseEntity<Object> findByName(
+            @RequestParam(name = "initPage") String initPage,
+            @RequestParam(name = "sizePage") String sizePage,
+            @RequestParam(name = "order", defaultValue = "0") String order
+    ) {
+        PageableSearchDTO dto = new PageableSearchDTO();
+        dto.setOrder(Integer.parseInt(order));
+        dto.setInitPage(Integer.parseInt(initPage));
+        dto.setSizePage(Integer.parseInt(sizePage));
+
+        Page<AnimatedCharacter> characterPage = animatedCharacterService.findAll(dto);
+        return ResponseEntity.ok(AnimatedCharacterMapper.INSTANCE.toPageDto(characterPage));
     }
 }
--- src/main/java/com/coderbyte/apprickmorty/controller/mapper/AnimatedCharacterMapper.java
+++ src/main/java/com/coderbyte/apprickmorty/controller/mapper/AnimatedCharacterMapper.java
@@ -1,11 +1,13 @@
 package com.coderbyte.apprickmorty.controller.mapper;
 
 import com.coderbyte.apprickmorty.controller.dto.AnimatedCharacterDTO;
+import com.coderbyte.apprickmorty.controller.dto.AnimatedCharacterPageDTO;
 import com.coderbyte.apprickmorty.model.AnimatedCharacter;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
 
 @Mapper
 public interface AnimatedCharacterMapper {
@@ -18,4 +20,6 @@ public interface AnimatedCharacterMapper {
 
     @InheritInverseConfiguration
     AnimatedCharacter toEntity(AnimatedCharacterDTO dto);
+
+    AnimatedCharacterPageDTO toPageDto(Page<AnimatedCharacter> characters);
 }
