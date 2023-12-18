aac210bae2c2a6ad006f23201456c51e45e9d293
andres.mera <feli041089@gmail.com>
2023-12-18 16:18:06 -0500

    dev-hexa : Modificacion de la configuracion de la application

--- src/main/java/com/coderbyte/apprickmorty/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/coderbyte/apprickmorty/infrastructure/config/ApplicationConfig.java
@@ -1,14 +1,22 @@
 package com.coderbyte.apprickmorty.infrastructure.config;
 
 import com.coderbyte.apprickmorty.application.services.AnimatedCharacterService;
+import com.coderbyte.apprickmorty.application.services.CalledTablesService;
+import com.coderbyte.apprickmorty.application.services.SystemErrorService;
+import com.coderbyte.apprickmorty.application.usecases.called.CreateCalledTableUserCaseImpl;
 import com.coderbyte.apprickmorty.application.usecases.character.CreateAnimatedCharacterUserCaseImpl;
 import com.coderbyte.apprickmorty.application.usecases.character.DeleteAnimatedCharacterUseCaseImpl;
 import com.coderbyte.apprickmorty.application.usecases.character.RetrieveAnimatedCharacterUseCaseImpl;
 import com.coderbyte.apprickmorty.application.usecases.character.UpdateAnimatedCharacterUseCaseImpl;
+import com.coderbyte.apprickmorty.application.usecases.system.CreateSystemErrorUserCaseImpl;
 import com.coderbyte.apprickmorty.domian.ports.out.AnimatedCharacterRepositoryPort;
+import com.coderbyte.apprickmorty.domian.ports.out.CalledTableRepositoryPort;
 import com.coderbyte.apprickmorty.domian.ports.out.ExternalServicePort;
+import com.coderbyte.apprickmorty.domian.ports.out.SystemErrorRepositoryPort;
 import com.coderbyte.apprickmorty.infrastructure.adapters.ExternalServiceAdapter;
 import com.coderbyte.apprickmorty.infrastructure.repositories.JpaAnimatedCharacterRepositoryAdapter;
+import com.coderbyte.apprickmorty.infrastructure.repositories.JpaCalledTablesRepositoryAdapter;
+import com.coderbyte.apprickmorty.infrastructure.repositories.JpaSystemErrorRepositoryAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,5 +40,29 @@ public class ApplicationConfig {
     @Bean
     public ExternalServicePort externalServicePort() {
         return new ExternalServiceAdapter();
+    }
+
+    @Bean
+    public CalledTablesService calledTableService(CalledTableRepositoryPort calledTableRepositoryPort) {
+        return new CalledTablesService(
+                new CreateCalledTableUserCaseImpl(calledTableRepositoryPort)
+        );
+    }
+
+    @Bean
+    public CalledTableRepositoryPort calledTableRepositoryPort(JpaCalledTablesRepositoryAdapter jpaCalledTablesRepositoryAdapter) {
+        return jpaCalledTablesRepositoryAdapter;
+    }
+
+    @Bean
+    public SystemErrorService systemErrorService(SystemErrorRepositoryPort systemErrorRepositoryPort) {
+        return new SystemErrorService(
+                new CreateSystemErrorUserCaseImpl(systemErrorRepositoryPort)
+        );
+    }
+
+    @Bean
+    public SystemErrorRepositoryPort systemErrorRepositoryPort(JpaSystemErrorRepositoryAdapter jpaSystemErrorRepositoryAdapter) {
+        return jpaSystemErrorRepositoryAdapter;
     }
 }
