247070f302f0d5e2d9ddea6e1a66a8e1db57e861
andres.mera <feli041089@gmail.com>
2023-12-18 16:17:19 -0500

    dev-hexa : Creacion y definicio delos puertos in de las entidades.

--- src/main/java/com/coderbyte/apprickmorty/domian/ports/in/called/CreateCalledTablesUseCase.java
+++ src/main/java/com/coderbyte/apprickmorty/domian/ports/in/called/CreateCalledTablesUseCase.java
@@ -1,4 +1,12 @@
 package com.coderbyte.apprickmorty.domian.ports.in.called;
 
+import com.coderbyte.apprickmorty.infrastructure.entities.CalledTablesDTO;
+import com.coderbyte.apprickmorty.infrastructure.entities.SystemErrorDTO;
+
 public interface CreateCalledTablesUseCase {
+    void postCalledTables(CalledTablesDTO calledTablesDTO);
+
+    void postCalledTablesWithError(CalledTablesDTO calledTablesDTO, SystemErrorDTO systemErrorDTO);
+
+    CalledTablesDTO addCalledTables(String parameter, Object data, String typeRequest);
 }
--- src/main/java/com/coderbyte/apprickmorty/domian/ports/in/system/CreateSystemErrorUseCase.java
+++ src/main/java/com/coderbyte/apprickmorty/domian/ports/in/system/CreateSystemErrorUseCase.java
@@ -1,4 +1,8 @@
 package com.coderbyte.apprickmorty.domian.ports.in.system;
 
+import com.coderbyte.apprickmorty.infrastructure.entities.SystemErrorDTO;
+
 public interface CreateSystemErrorUseCase {
+    SystemErrorDTO postSystemError(SystemErrorDTO systemErrorDTO);
+    SystemErrorDTO addSystemError(String code, String status, String message);
 }
