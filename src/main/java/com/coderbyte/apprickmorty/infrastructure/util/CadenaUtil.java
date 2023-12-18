2fe1b6e7dad46bacc9d6822a319d92c1918f6571
andres.mera <feli041089@gmail.com>
2023-12-18 16:14:17 -0500

    dev-hexa : Entidad de utilidades para las cadenas de caracteres.

---
+++ src/main/java/com/coderbyte/apprickmorty/infrastructure/util/CadenaUtil.java
@@ -0,0 +1,16 @@
+package com.coderbyte.apprickmorty.infrastructure.util;
+
+import com.github.cliftonlabs.json_simple.JsonObject;
+
+public class CadenaUtil {
+
+    private CadenaUtil() {
+        throw new IllegalStateException(CadenaUtil.class.toString());
+    }
+
+    public static String convertBodyToJson(String attributeName, Object body) {
+        JsonObject json = new JsonObject();
+        json.put(attributeName, body);
+        return json.toString();
+    }
+}
