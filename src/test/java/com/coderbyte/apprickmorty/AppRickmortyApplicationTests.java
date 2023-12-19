b20d5f0f2651ce91e0549af36fbccea12f21a474
andres.mera <feli041089@gmail.com>
2023-12-19 15:50:05 -0500

    dev-hexa : Primer test del sistema.

--- src/test/java/com/coderbyte/apprickmorty/AppRickmortyApplicationTests.java
+++ src/test/java/com/coderbyte/apprickmorty/AppRickmortyApplicationTests.java
@@ -1,13 +1,17 @@
 package com.coderbyte.apprickmorty;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.AssertionErrors;
 
 @SpringBootTest
 class AppRickmortyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        AppRickmortyApplication myClass = new AppRickmortyApplication();
+        Assertions.assertNotNull(myClass);  // JUnit assertion
+    }
 
 }
