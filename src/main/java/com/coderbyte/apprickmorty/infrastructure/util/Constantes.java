c083b8f92f558ee6e81ed1fb0a20795ec07e8778
andres.mera <feli041089@gmail.com>
2023-12-19 14:09:10 -0500

    dev-hexa : Creacion de entidad de valores Constantes.java.

---
+++ src/main/java/com/coderbyte/apprickmorty/infrastructure/util/Constantes.java
@@ -0,0 +1,10 @@
+package com.coderbyte.apprickmorty.infrastructure.util;
+
+public class Constantes {
+
+    private Constantes() {
+        throw new IllegalStateException(Constantes.class.toString());
+    }
+
+    public static final String ORDER = "order";
+}
