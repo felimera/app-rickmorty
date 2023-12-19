987c17bca41cd1761224250634468079d696cc01
andres.mera <feli041089@gmail.com>
2023-12-19 14:35:28 -0500

    dev-hexa : Nuevo servicio para la paginacion.

--- src/main/java/com/coderbyte/apprickmorty/domian/ports/in/character/RetrieveAnimatedCharacterUseCase.java
+++ src/main/java/com/coderbyte/apprickmorty/domian/ports/in/character/RetrieveAnimatedCharacterUseCase.java
@@ -1,6 +1,8 @@
 package com.coderbyte.apprickmorty.domian.ports.in.character;
 
 import com.coderbyte.apprickmorty.infrastructure.entities.AnimatedCharacterDTO;
+import com.coderbyte.apprickmorty.infrastructure.entities.pages.AnimatedCharacterPageDTO;
+import com.coderbyte.apprickmorty.infrastructure.entities.parameter.PageableSearchDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,6 @@ public interface RetrieveAnimatedCharacterUseCase {
     Optional<AnimatedCharacterDTO> getAnimatedCharacter(Long id);
 
     List<AnimatedCharacterDTO> getAllAnimatedCharacters(Integer order);
+
+    AnimatedCharacterPageDTO getDtoPage(PageableSearchDTO dto);
 }
