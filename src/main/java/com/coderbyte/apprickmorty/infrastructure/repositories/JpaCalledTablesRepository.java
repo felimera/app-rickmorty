834f4413293146f2838588fe6e1f152f350898db
andres.mera <feli041089@gmail.com>
2023-12-19 13:54:22 -0500

    dev-hexa : Modificacion del nombre.

--- src/main/java/com/coderbyte/apprickmorty/infrastructure/repositories/JpaCalledTablesRepository.java
+++ src/main/java/com/coderbyte/apprickmorty/infrastructure/repositories/JpaCalledTablesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaCalledTablesRespository extends JpaRepository<CalledTablesEntity, Long> {
+public interface JpaCalledTablesRepository extends JpaRepository<CalledTablesEntity, Long> {
 }
