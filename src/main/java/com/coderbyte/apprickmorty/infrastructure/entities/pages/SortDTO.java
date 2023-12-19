a409ad39cd0fa97f2115d364bcd08224f681ca43
andres.mera <feli041089@gmail.com>
2023-12-19 14:14:50 -0500

    dev-hexa : Creacion de dto para la paginacion.

---
+++ src/main/java/com/coderbyte/apprickmorty/infrastructure/entities/pages/SortDTO.java
@@ -0,0 +1,12 @@
+package com.coderbyte.apprickmorty.infrastructure.entities.pages;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SortDTO {
+    private Boolean empty;
+    private Boolean sorted;
+    private Boolean unsorted;
+}
